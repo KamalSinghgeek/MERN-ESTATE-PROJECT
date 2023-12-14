5bb73178f56ef370d7cf307f92e37dbae8da743b
Kamal Singh <kamalsingh23056@gmail.com>
2023-12-15 03:33:58 +0530

    Add delete User functionality (Permanently Delete User from Database)

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import {useState, useEffect, useRef } from 'react';
 import {useSelector} from 'react-redux';
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
 import { app } from '../firebase';
-import { updateUserStart,updateUserSuccess, updateUserFailure } from '../redux/user/userSlice';
+import { updateUserStart,updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserFailure, deleteUserSuccess } from '../redux/user/userSlice.js';
 import { useDispatch } from 'react-redux';
 
 export default function Profile() {
@@ -72,6 +72,23 @@ const handleSubmit =async (e) => {
   }
 };
 
+const handleDeleteUser = async () => {
+  try {
+    dispatch(deleteUserStart());
+    const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+      method: 'DELETE',
+    });
+    const data = await res.json();
+    if(data.success === false) {
+      dispatch(deleteUserFailure(data.message));
+      return;
+    }
+    dispatch(deleteUserSuccess(data))
+  } catch (error) {
+    dispatch(deleteUserFailure(error.message));
+  }
+};
+
 return (
     <div className='p-3 max-w-lg mx-auto'>
     <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -119,7 +136,7 @@ return (
     disabled:opacity-80'>{loading ?'Loading...' : 'Update'}</button>
     </form>
     <div className='flex justify-between mt-5' >
-    <span className='text-red-700 cursor-pointer'>Delete account</span>
+    <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete account</span>
     <span className='text-red-700 cursor-pointer'>Sign out</span>
     
     </div>
