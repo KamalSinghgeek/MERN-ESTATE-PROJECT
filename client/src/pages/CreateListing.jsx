6a4e127233d112965dc9e4b3de7e7fe33e157494
Kamal Singh <kamalsingh23056@gmail.com>
2023-12-17 03:52:46 +0530

    Complete Upload Listing Images functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
+import {app} from '../firebase';
 
 export default function CreateListing() {
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
+   imageUrls: [],
+  });
+  const [imageUploadError, setImageUploadError]= useState(false);
+  const [uploading, setUploading]= useState(false);
+  console.log(formData)
+  const handleImageSubmit = (e) => {
+    if(files.length > 0 && files.length + formData.imageUrls.length <7) {
+      setUploading(true);
+      setImageUploadError(false);
+      const promises=[];
+
+      for (let i=0; i<files.length; i++)
+      {
+promises.push(storeImage(files[i]));
+      }
+      Promise.all(promises).then((urls) => {
+        setFormData({
+          ...formData, imageUrls: formData.imageUrls.concat(urls),
+        });
+
+           setImageUploadError(false);
+           setUploading(false);
+      }).catch((err)=> {
+         setImageUploadError('Image upload failed (2 MB max per image)');
+         setUploading(false);
+        });
+  }
+  else {
+    setImageUploadError('You can only upload 6 images per listing')
+    setUploading(false);
+  }
+};
+
+const storeImage = async (file) => {
+ return new Promise((resolve, reject) => {
+  const storage = getStorage(app);
+  const fileName= new Date().getTime() + file.name;
+  const storageRef = ref(storage, fileName);
+  const upoloadTask = uploadBytesResumable(storageRef, file);
+  upoloadTask.on(
+    "state_changed",
+    (snapshot) => {
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes)*100;
+    console.log(`Upload is ${progress}% done`);
+    },
+    (error) => {
+      reject(error);
+    },
+    () => {
+      getDownloadURL(upoloadTask.snapshot.ref).then((downloadURL) => {
+        resolve(downloadURL);
+      });
+    }
+  );
+  });
+};
+
+const handleRemoveImage = (index) => {
+  setFormData({
+    ...formData, imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+
+  });
+};
+
   return (
     <main className='p-3 max-w-4xl mx-auto'>
         <h1 className='text-3xl font-semibold text-center my-7'>Create a Listing</h1>
@@ -70,9 +138,20 @@ export default function CreateListing() {
     <span className='font-normal text-gray-600 ml-2'>The first image  will be the cover (max 6)</span>
     </p>
     <div className='flex gap-4'>
-    <input className='p-3 border border-gray-300 rounded w-full' type="file" id='image' accept='image/*' multiple />
-    <button className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'> Upload </button>
-    </div>    
+    <input onChange={(e) => setFiles(e.target.files)} 
+    className='p-3 border border-gray-300 rounded w-full' type="file" id='image' accept='image/*' multiple />
+    <button type='button' disabled={uploading} onClick={handleImageSubmit} className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'> {uploading ? 'Uploading...' : 'Upload'}</button>
+    </div>   
+    <p className='text-red-700 text-sm'>{imageUploadError && imageUploadError}</p> 
+    {
+      formData.imageUrls.length > 0 && formData.imageUrls.map((url, index) => (
+        <div key={url} className='flex justify-between p-3 border items-center'>
+        <img src={url} alt='listing image' className='w-20 h-20 object-contain rounded-lg' />
+       <button type='button' onClick={() => handleRemoveImage(index)} className='p-3 text-red-700 rounded-lg uppercase hover:opacity-60'>Delete</button>
+        </div>
+
+      ))
+    }
     <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Create Listing</button>
     </div>
 
